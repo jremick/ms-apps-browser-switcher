886e9b90fbd769bfb0579ec74e6fb72137a4ab4f
Jarel <jremick@jremick.com>
2023-08-12 08:11:35 +1000

    v2 using ctypes
    Much more efficient version using an event hook approach to check the active window instead of polling.

--- default-browser-switcher.py
+++ default-browser-switcher.py
@@ -1,97 +1,127 @@
-import time
-import win32gui
-import win32process
+import ctypes
+import ctypes.wintypes
 import psutil
 import subprocess
-import os
+import atexit
+import signal
 
-# Set the debug mode to false (by default)
-DEBUG_MODE = True
+# Configuration
+APPS = [("teams.exe", "Microsoft Teams"), ("outlook.exe", "Outlook"), ("chrome.exe", "Chrome")] # Configure apps to monitor for
+BROWSERS = {'chrome': 'ChromeHTML', 'edge': 'MSEdgeHTM'} # First browser is set for monitored apps
+PROTOCOLS = ['http', 'https', '.htm', '.html', '.xhtml', 'ftp', '.svg', '.webp', '.mhtml', '.shtml', '.xml']
 
-# Get the current process
-process = psutil.Process(os.getpid())
+DEBUG_MODE = False
 
-# Set the process to idle priority class
-process.nice(psutil.IDLE_PRIORITY_CLASS)
-
-# Define the applications to monitor
-apps = [("teams.exe", "Microsoft Teams"), ("outlook.exe", "Outlook"), ("chrome.exe", "Chrome")]
-
-# Initialize dictionaries to keep track of which applications were active first
-app_active_first_check = {app[1]: 0 for app in apps}
+# Global variables
 current_default_browser = None
+hook = None 
 
-# Initialize a set instead of a list for faster lookups
-app_process_names = {app[0].lower() for app in apps}
 
-# Function to check if a process is running and get the PID of the active window
-def get_running_processes_and_active_window_pid():
-    proc_info = [(proc.info['name'].lower(), proc.info['pid']) for proc in psutil.process_iter(['name', 'pid'])]
-    running_processes = [(name, pid) for name, pid in proc_info if name in app_process_names]
-    # Get the active window PID only if the current process matches one of the monitored processes
-    active_window_handle = win32gui.GetForegroundWindow()
-    active_window_pid = win32process.GetWindowThreadProcessId(active_window_handle)[1] if any(pid == win32process.GetWindowThreadProcessId(active_window_handle)[1] for name, pid in running_processes) else None
-    return running_processes, active_window_pid
+# Unhook the Windows event
+def unhook_win_event():
+    global hook
+    if hook:
+        ctypes.windll.user32.UnhookWinEvent(hook)
 
-# Function to execute a command and return its output
+atexit.register(unhook_win_event)  # Register the unhook function to be called on exit
+
+
+# Get the PID of the active window
+def get_active_window_pid():
+    active_window_handle = ctypes.windll.user32.GetForegroundWindow()
+    active_window_pid = ctypes.wintypes.DWORD()
+    ctypes.windll.user32.GetWindowThreadProcessId(active_window_handle, ctypes.byref(active_window_pid))
+    return active_window_pid.value
+
+
+# Execute a given shell command, capturing and handling errors
 def execute_command(command):
-    output = subprocess.run(command, shell=True, capture_output=True)
-    if output.stderr:
+    try:
+        subprocess.run(command, shell=True, capture_output=True, check=True)
+    except subprocess.CalledProcessError as e:
         if DEBUG_MODE:
-            print('Error:', output.stderr.decode('utf-8'))
+            print(f"Error executing command '{command}': {e.stderr.decode('utf-8')}")
+    except Exception as e:
+        if DEBUG_MODE:
+            print(f"Unexpected error executing command '{command}': {e}")
 
-# Function to change the default browser
-def change_default_browser(browser, current_default_browser):
+
+# Change the default browser to the given one, identified by its name
+def change_default_browser(browser):
+    global current_default_browser
     browser = browser.lower()
     if browser == current_default_browser:
-        return current_default_browser  # return current_default_browser if it is already set correctly
-
-    prog_id = {'chrome': 'ChromeHTML', 'edge': 'MSEdgeHTML'}.get(browser)
+        return
+    prog_id = BROWSERS.get(browser)
     if prog_id:
-        for protocol in ['http', 'https', '.htm', '.html']:
+        for protocol in PROTOCOLS:
             command = 'SetUserFTA {} {}'.format(protocol, prog_id)
             execute_command(command)
-
-        current_default_browser = browser  # update the current default_browser
-    elif current_default_browser is None:
+        current_default_browser = browser
         if DEBUG_MODE:
-            print('Unsupported browser:', browser)
-
-    return current_default_browser  # return the updated current_default_browser
+            print(f'Changed default browser to {browser}. Program ID: {prog_id}')
 
 
-active_window_pid_prev = None
-active_window_check_interval = 1.5  # initial sleep time
-
-if __name__ == '__main__':
-    while True:
-        app_running = False
-        running_processes, active_window_pid = get_running_processes_and_active_window_pid()
-
-        for process_name, app_name in apps:
-            app_processes = [(name, pid) for name, pid in running_processes if name == process_name.lower()]
-
-            if app_processes:
-                app_running = True
-                if any(pid == active_window_pid for name, pid in app_processes):
-                    if time.time() - app_active_first_check[app_name] > 5:  # app has been active for more than 5 seconds
-                        if DEBUG_MODE:
-                            print('{} is currently active'.format(app_name))
-                        app_active_first_check[app_name] = time.time()
-                        current_default_browser = change_default_browser('chrome', current_default_browser)
-                    break
-        else:  # if the loop completes without breaking (i.e., no app in the list is in focus)
-            if time.time() - max(app_active_first_check.values()) > 5:  # no app has been active for more than 5 seconds
+# Callback function that handles the foreground window change event
+def on_foreground_window_change(hWinEventHook, event, hwnd, idObject, idChild, dwEventThread, dwmsEventTime):
+    active_window_pid = get_active_window_pid()
+    for process_name, app_name in APPS:
+        try:
+            process = psutil.Process(active_window_pid)
+            if process_name.lower() == process.name().lower():
                 if DEBUG_MODE:
-                    print('Other application is currently active')
-                app_active_first_check = {app[1]: 0 for app in apps}
-                current_default_browser = change_default_browser('edge', current_default_browser)
+                    print(f"{app_name} is currently active")
+                change_default_browser('chrome')
+                return
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            continue
 
-        # If the active window PID hasn't changed, increment the sleep time to reduce CPU usage
-        if active_window_pid == active_window_pid_prev:
-            active_window_check_interval = min(active_window_check_interval + 0.1, 3)  # max sleep time of 2 seconds
-        else:
-            active_window_check_interval = 1.5  # reset sleep time if the active window changes
+    if DEBUG_MODE:
+        print('Other application is currently active')
+    change_default_browser('edge')
 
-        active_window_pid_prev = active_window_pid
-        time.sleep(active_window_check_interval)  # check for active window changes every second
+
+# Handle the exit signal to gracefully unhook the event and exit the script
+def exit_handler(signal, frame):
+    user32.UnhookWinEvent(hook)
+    ctypes.windll.user32.PostQuitMessage(0)  # Post a quit message to exit the loop
+    print("Exiting...")
+
+
+# Main function
+if __name__ == '__main__':
+    # Register the exit handler to handle the SIGINT signal (Ctrl+C)
+    signal.signal(signal.SIGINT, exit_handler)
+
+    # Define a Windows function callback type
+    WinEventProc = ctypes.WINFUNCTYPE(None, ctypes.wintypes.HANDLE, ctypes.wintypes.DWORD, ctypes.wintypes.HWND, ctypes.wintypes.LONG, ctypes.wintypes.LONG, ctypes.wintypes.DWORD, ctypes.wintypes.DWORD)
+    
+    # Create the event callback using the defined function type
+    event_callback = WinEventProc(on_foreground_window_change)
+    
+    # Access the user32 library
+    user32 = ctypes.windll.user32
+    
+    # Set the Windows event hook to listen to the foreground window change event
+    hook = user32.SetWinEventHook(
+        3, # EVENT_SYSTEM_FOREGROUND
+        3, # EVENT_SYSTEM_FOREGROUND
+        0,
+        event_callback,
+        0,
+        0,
+        0 # WINEVENT_OUTOFCONTEXT
+    )
+
+    # Create a message object to handle the messages in the queue
+    msg = ctypes.wintypes.MSG()
+
+    # Main message loop
+    try:
+        while ctypes.windll.user32.GetMessageW(ctypes.byref(msg), 0, 0, 0) != 0:
+            # Translate and dispatch the messages from the queue
+            ctypes.windll.user32.TranslateMessage(ctypes.byref(msg))
+            ctypes.windll.user32.DispatchMessageW(ctypes.byref(msg))
+    except KeyboardInterrupt:
+        # Call the exit handler if a keyboard interrupt is detected
+        exit_handler(None, None)
